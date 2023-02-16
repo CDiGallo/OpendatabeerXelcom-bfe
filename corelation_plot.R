bc7ad05a1e4bb393a7319ecd33748071e430dddf
CDiGallo <86839683+CDiGallo@users.noreply.github.com>
2023-02-16 12:49:00 +0100

    Add files via upload

--- corelation_plot.R
+++ corelation_plot.R
@@ -54,16 +54,19 @@ df_joined$Leistung.in.kW.pro.100000.Einwohner <-as.integer(df_joined$Leistung.in
 df_joined$Anzahl.geförderter.EIV.Anlagen.pro.100000.Einwohner <- as.integer(df_joined$Anzahl.geförderter.EIV.Anlagen.pro.100000.Einwohner)
 
 
-e <- ggplot(df_joined, aes(Leistung.in.kW.pro.100000.Einwohner,Total.exkl..MWST))
-e + geom_point()
+e <- ggplot(df_joined, aes(Leistung.in.kW.pro.100000.Einwohner,Total.exkl..MWST, color =Periode, size =19))
+e + geom_point()+ xlab("Neu installierte  Leistung in KWh pro 100000 Einwohnenden") +ylab("Totalpreis (Rp./KWh)") +ggtitle("Totalstrompreis und installierte Leistung")
 
-e <- ggplot(df_joined, aes(df_joined$Anzahl.geförderter.EIV.Anlagen.pro.100000.Einwohner,Total.exkl..MWST))
+e <- ggplot(df_joined, aes(df_joined$Anzahl.geförderter.EIV.Anlagen.pro.100000.Einwohner,Total.exkl..MWST, color =Periode))
 e + geom_point()
 
 
 dfjoined$Energielieferungkosten
-e <- ggplot(df_joined, aes(Leistung.in.kW.pro.100000.Einwohner,Energielieferungkosten))
+e <- ggplot(df_joined, aes(Leistung.in.kW.pro.100000.Einwohner,Energielieferungkosten, color=Periode))
 e + geom_point()
+
+e <- ggplot(df_joined, aes(Leistung.in.kW.pro.100000.Einwohner,Energielieferungkosten, color=Periode, size=19))
+e + geom_point() + xlab("Neu installierte  Leistung in KWh pro 100000 Einwohnenden") +ylab("Energielieferpreis (Rp./KWh)") +ggtitle("Lieferpreise und installierte Leistung")
 
 
 #Ok, this does not work. Problem is, maaaaaaaaybeeeee: dass alles eigentlihc nur mit dem Jahr korreliert. ich muss also innerhalb eines Jahres schauen
@@ -168,5 +171,36 @@ df_joined <- df_joined %>% mutate(normed_kwh= (Leistung.in.kW.pro.100000.Einwohn
 df_joined
 
 
-e <- ggplot(df_joined, aes(normed_kwh,normed_cost, color=Periode))
-e + geom_point()
+e <- ggplot(df_joined, aes(normed_kwh,normed_cost, color=Periode, size= 19))
+e + geom_point() + xlab("Neu isntallierte Leistung in KWh pro 100000 Einwohnenden, standardisiert") +ylab("standardisierter Totalpreis (Rp./KWh)") + ggtitle("standarisierte total Lieferpreise und installierte Leistung")
+
+
+t + facet_wrap(vars(Kanton)) +theme_light()  + theme(axis.text.x=element_text(angle = 45, vjust = 0.5))#, cols = vars(fl))
+
+
+
+
+
+
+
+#Nach Lieferpreise standardisieren
+
+df_joined$Leistung.in.kW.pro.100000.Einwohner
+df_joined %>% group_by(Periode) %>% summarise(mean(Leistung.in.kW.pro.100000.Einwohner))
+
+df_norming <-  df_joined %>% group_by(Periode) %>% summarise( mean_kwh=mean(Leistung.in.kW.pro.100000.Einwohner), sd_kwh=sd(Leistung.in.kW.pro.100000.Einwohner), mean_cost=mean(Energielieferungkosten), sd_cost=sd(Energielieferungkosten))
+
+df_joined <- left_join(df_joined, df_norming, by="Periode")
+
+df_joined
+df_joined <- df_joined %>% mutate(normed_kwh= (Leistung.in.kW.pro.100000.Einwohner-mean_kwh)/sd_kwh, normed_cost=(Total.exkl..MWST-mean_cost)/sd_cost)
+
+df_joined
+
+
+e <- ggplot(df_joined, aes(normed_kwh,normed_cost, color=Periode, size=19))
+e + geom_point() + xlab("Neu installierte  Leistung in KWh pro 100000 Einwohnenden, standardisiert") +ylab("Energielieferpreis (Rp./KWh), standardisiert") +ggtitle("Lieferpreise und installierte Leistung")
+
+
+
+
